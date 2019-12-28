0016a9c495e4428a56f300497c0944f716de8459
Allan Chappell <generalredneck@gmail.com>
2019-12-27 22:49:16 -0600

    Implemented decendents.

--- farm_livestock_pedigree.js
+++ farm_livestock_pedigree.js
@@ -1,12 +1,12 @@
-
+(function ($) {
 // Set default width and height, calculate ratio
 var default_width = 960;
 var default_height = 695;
 var default_ratio = default_width / default_height;
 
-var margin = {top: 0, right: 320, bottom: 0, left: 0},
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
     width = default_width - margin.left - margin.right,
-    height = default_height - margin.top - margin.bottom;
+    height = default_height;
 // Determine current size, which determines vars
 function set_vars() {
   //alert('setting vars')
@@ -14,89 +14,456 @@ function set_vars() {
   current_height = window.innerHeight;
 
   current_ratio = current_width / current_height;
+  offset = $(".farm-pedigree").offset();
 
-  // Check if height is limiting factor
-  if ( current_ratio > default_ratio ){
-    h = current_height;
-    w = h * default_ratio;
-  // Else width is limiting
-  } else {
-    w = current_width;
-    h = w / default_ratio;
-  }
-
+  console.log(offset);
+  w = current_width;
+  h = current_height - offset.top - 5;
   // Set new width and height based on graph dimensions
   width = w - margin.left - margin.right;
   height = h - margin.top - margin.bottom;
 
 };
 
-set_vars();
+var boxWidth = 150,
+    boxHeight = 100,
+    nodeWidth = 200,
+    nodeHeight = 300,
 
-function drawGraphic() {
-  var tree = d3.layout.tree()
-      .separation(function(a, b) { return a.parent === b.parent ? 1 : .5; })
-      .children(function(d) { return d.parents; })
-      .size([height, width]);
+    // duration of transitions in ms
+    duration = 750,
+
+    // d3 multiplies the node size by this value
+    // to calculate the distance between nodes
+    separation = .60;
+/**
+ * For the sake of the examples, I want the setup code to be at the top.
+ * However, since it uses a class (Tree) which is defined later, I wrap
+ * the setup code in a function at call it at the end of the example.
+ * Normally you would extract the entire Tree class defintion into a
+ * separate file and include it before this script tag.
+ */
+function setup() {
+  // Setup zoom and pan
+  var zoom = d3.behavior.zoom()
+    .scaleExtent([.1,1])
+    .on('zoom', function(){
+      svg.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+    })
+    // Offset so that first pan and zoom does not jump back to the origin
+    .translate([400, 200]);
 
   var svg = d3.select(".farm-pedigree").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr('width', width)
+    .attr('height', height)
+    .call(zoom)
+    .append('g')
 
-  var json = Drupal.settings.farm_livestock_pedigree.pedigree_data;
-  var nodes = tree.nodes(json);
+    // Left padding of tree so that the whole root node is on the screen.
+    // TODO: find a better way
+    .attr("transform", "translate(400,200)");
 
-  var link = svg.selectAll(".link")
-      .data(tree.links(nodes))
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", elbow);
 
-  var node = svg.selectAll(".node")
-      .data(nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+  // One tree to display the ancestors
+  var ancestorTree = new Tree(svg, 'ancestor', 1);
+  ancestorTree.children(function(person){
+    // If the person is collapsed then tell d3
+    // that they don't have any ancestors.
+    if(person.collapsed){
+      return;
+    } else {
+      return person._parents;
+    }
+  });
 
-  node.append("text")
-      .attr("class", "name")
-      .attr("x", 8)
-      .attr("y", -6)
-      .text(function(d) {
-        var sex = "";
-        if (d.sex == 'M') {
-          sex = "♂";
-        }
-        if (d.sex == 'F') {
-          sex = "♀";
-        }
-        return sex + " " + d.name;
+  // Use a separate tree to display the descendants
+  var descendantsTree = new Tree(svg, 'descendant', -1);
+  descendantsTree.children(function(person){
+    if(person.collapsed){
+      return;
+    } else {
+      return person._children;
+    }
+  });
+
+  json = Drupal.settings.farm_livestock_pedigree.pedigree_data;
+
+  // D3 modifies the objects by setting properties such as
+  // coordinates, parent, and children. Thus the same node
+  // node can't exist in two trees. But we need the root to
+  // be in both so we create proxy nodes for the root only.
+  var ancestorRoot = rootProxy(json);
+  var descendantRoot = rootProxy(json);
+
+  // Start with only the first few generations of ancestors showing
+  // 1
+
+  if (ancestorRoot._parents !== undefined) {
+    ancestorRoot._parents.forEach(function(parents){
+      if (parents._parents !== undefined) {
+        // 2
+        parents._parents.forEach(function(parents){
+          if (parents._parents !== undefined) {
+            // 3
+            parents._parents.forEach(function(parents){
+              if (parents._parents !== undefined) {
+                // 4
+                parents._parents.forEach(function(parents){
+                  if (parents._parents !== undefined) {
+                    // 5
+                    parents._parents.forEach(collapse);
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+
+  // Start with only one generation of descendants showing
+  if (ancestorRoot._children !== undefined) {
+    descendantRoot._children.forEach(collapse);
+  }
+
+  // Set the root nodes
+  ancestorTree.data(ancestorRoot);
+  descendantsTree.data(descendantRoot);
+
+  // Draw the tree
+  ancestorTree.draw(ancestorRoot);
+  descendantsTree.draw(descendantRoot);
+}
+
+function rootProxy(root){
+  return {
+    name: root.name,
+    sex: root.sex,
+    type: root.type,
+    dob: root.dob,
+    id: root.id,
+    x0: 0,
+    y0: 0,
+    _children: root._children,
+    _parents: root._parents,
+    collapsed: false
+  };
+}
+/**
+ * Shared code for drawing ancestors or descendants.
+ * `selector` is a class that will be applied to links
+ * and nodes so that they can be queried later when
+ * the tree is redrawn.
+ * `direction` is either 1 (forward) or -1 (backward).
+ */
+var Tree = function(svg, selector, direction){
+  this.svg = svg;
+  this.selector = selector;
+  this.direction = direction;
+
+  this.tree = d3.layout.tree()
+
+      // Using nodeSize we are able to control
+      // the separation between nodes. If we used
+      // the size parameter instead then d3 would
+      // calculate the separation dynamically to fill
+      // the available space.
+      .nodeSize([nodeWidth, nodeHeight])
+
+      // By default, cousins are drawn further apart than siblings.
+      // By returning the same value in all cases, we draw cousins
+      // the same distance apart as siblings.
+      .separation(function(){
+        return separation;
+      });
+};
+
+/**
+ * Set the `children` function for the tree
+ */
+Tree.prototype.children = function(fn){
+  this.tree.children(fn);
+  return this;
+};
+/**
+ * Set the root of the tree
+ */
+Tree.prototype.data = function(data){
+  this.root = data;
+  return this;
+};
+
+/**
+ * Draw/redraw the tree
+ */
+Tree.prototype.draw = function(source){
+  if(this.root){
+    var nodes = this.tree.nodes(this.root),
+        links = this.tree.links(nodes);
+    this.drawLinks(links, source);
+    this.drawNodes(nodes, source);
+  } else {
+    throw new Error('Missing root');
+  }
+  return this;
+};
+/**
+ * Draw/redraw the connecting lines
+ */
+Tree.prototype.drawLinks = function(links, source){
+
+  var self = this;
+
+  // Update links
+  var link = self.svg.selectAll("path.link." + self.selector)
+
+      // The function we are passing provides d3 with an id
+      // so that it can track when data is being added and removed.
+      // This is not necessary if the tree will only be drawn once
+      // as in the basic example.
+      .data(links, function(d){ return d.target.id; });
+
+  // Add new links
+  // Transition new links from the source's
+  // old position to the links final position
+  link.enter().append("path")
+      .attr("class", "link " + self.selector)
+      .attr("d", function(d) {
+        var o = {x: source.x0, y: self.direction * (source.y0 + boxWidth/2)};
+        return transitionElbow({source: o, target: o});
       });
 
-  node.append("text")
-      .attr("x", 8)
-      .attr("y", 8)
-      .attr("dy", ".71em")
-      .attr("class", "about type")
-      .text(function(d) { return "Breed: " + d.type; });
+  // Update the old links positions
+  link.transition()
+      .duration(duration)
+      .attr("d", function(d){
+        return elbow(d, self.direction);
+      });
 
-  node.append("text")
-      .attr("x", 8)
-      .attr("y", 8)
-      .attr("dy", "1.86em")
-      .attr("class", "about lifespan")
-      .text(function(d) { return "DOB: " + d.dob; });
+  // Remove any links we don't need anymore
+  // if part of the tree was collapsed
+  // Transition exit links from their current position
+  // to the source's new position
+  link.exit()
+      .transition()
+      .duration(duration)
+      .attr("d", function(d) {
+        var o = {x: source.x, y: self.direction * (source.y + boxWidth/2)};
+        return transitionElbow({source: o, target: o});
+      })
+      .remove();
+};
 
-  function elbow(d, i) {
-    return "M" + d.source.y + "," + d.source.x
-        + "H" + d.target.y + "V" + d.target.x
-        + (d.target.children ? "" : "h" + margin.right);
+/**
+ * Draw/redraw the person boxes.
+ */
+Tree.prototype.drawNodes = function(nodes, source){
+
+  var self = this;
+
+  // Update nodes
+  var node = self.svg.selectAll("g.person." + self.selector)
+
+      // The function we are passing provides d3 with an id
+      // so that it can track when data is being added and removed.
+      // This is not necessary if the tree will only be drawn once
+      // as in the basic example.
+      .data(nodes, function(person){ return person.id; });
+
+  // Add any new nodes
+  var nodeEnter = node.enter().append("g")
+      .attr("class", "person " + self.selector)
+
+      // Add new nodes at the right side of their child's box.
+      // They will be transitioned into their proper position.
+      .attr('transform', function(person){
+        return 'translate(' + (self.direction * (source.y0 + boxWidth/2)) + ',' + source.x0 + ')';
+      })
+      .on('click', function(person){
+        self.togglePerson(person);
+      });
+  // Draw the rectangle person boxes.
+  // Start new boxes with 0 size so that
+  // we can transition them to their proper size.
+  nodeEnter.append("rect")
+      .attr("class", function(d) {
+        if (d.sex == 'M') {
+          return 'male';
+        }
+        if (d.sex == 'F') {
+          return 'female';
+        }
+        return '';
+      })
+      .attr({
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+      });
+  // Draw the person's name and position it inside the box
+  nodeEnter.append("text")
+      .attr("dx", 0)
+      .attr("dy", -35)
+      .attr("text-anchor", "start")
+      .attr('class', 'name')
+      .text(function(d) {
+        return d.name;
+      })
+      .style('fill-opacity', 0);
+  nodeEnter.append("text")
+      .attr("dx", 0)
+      .attr("dy", -15)
+      .attr("text-anchor", "start")
+      .attr('class', 'breed')
+      .text(function(d) {
+        return 'Breed: ' + d.type;
+      })
+      .style('fill-opacity', 0);
+  nodeEnter.append("text")
+      .attr("dx", 0)
+      .attr("dy", 5)
+      .attr("text-anchor", "start")
+      .attr('class', 'dob')
+      .text(function(d) {
+        return 'DOB: ' + d.dob;
+      })
+      .style('fill-opacity', 0);
+
+  // Update the position of both old and new nodes
+  var nodeUpdate = node.transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + (self.direction * d.y) + "," + d.x + ")"; });
+
+  // Grow boxes to their proper size
+  nodeUpdate.select('rect')
+      .attr({
+        x: -(boxWidth/2),
+        y: -(boxHeight/2),
+        width: boxWidth,
+        height: boxHeight
+      });
+
+  // Move text to it's proper position
+  nodeUpdate.select('text.name')
+      .attr("dx", -(boxWidth/2) + 10)
+      .style('fill-opacity', 1);
+  nodeUpdate.select('text.breed')
+      .attr("dx", -(boxWidth/2) + 10)
+      .style('fill-opacity', 1);
+  nodeUpdate.select('text.dob')
+      .attr("dx", -(boxWidth/2) + 10)
+      .style('fill-opacity', 1);
+
+  // Remove nodes we aren't showing anymore
+  var nodeExit = node.exit()
+      .transition()
+      .duration(duration)
+
+      // Transition exit nodes to the source's position
+      .attr("transform", function(d) { return "translate(" + (self.direction * (source.y + boxWidth/2)) + "," + source.x + ")"; })
+      .remove();
+
+  // Shrink boxes as we remove them
+  nodeExit.select('rect')
+      .attr({
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0
+      });
+
+  // Fade out the text as we remove it
+  nodeExit.select('text')
+      .style('fill-opacity', 0)
+      .attr('dx', 0);
+
+  // Stash the old positions for transition.
+  nodes.forEach(function(person) {
+    person.x0 = person.x;
+    person.y0 = person.y;
+  });
+
+};
+/**
+ * Update a person's state when they are clicked.
+ */
+Tree.prototype.togglePerson = function(person){
+
+  // Don't allow the root to be collapsed because that's
+  // silly (it also makes our life easier)
+  if(person === this.root){
+    return;
+  }
+
+  // Non-root nodes
+  else {
+
+    if(person.collapsed){
+      person.collapsed = false;
+    } else {
+      collapse(person);
+    }
+
+    this.draw(person);
+  }
+};
+/**
+ * Collapse person (hide their ancestors). We recursively
+ * collapse the ancestors so that when the person is
+ * expanded it will only reveal one generation. If we don't
+ * recursively collapse the ancestors then when
+ * the person is clicked on again to expand, all ancestors
+ * that were previously showing will be shown again.
+ * If you want that behavior then just remove the recursion
+ * by removing the if block.
+ */
+function collapse(person){
+  person.collapsed = true;
+  if(person._parents){
+    person._parents.forEach(collapse);
+  }
+  if(person._children){
+    person._children.forEach(collapse);
   }
 }
-drawGraphic();
 
+/**
+ * Custom path function that creates straight connecting
+ * lines. Calculate start and end position of links.
+ * Instead of drawing to the center of the node,
+ * draw to the border of the person profile box.
+ * That way drawing order doesn't matter. In other
+ * words, if we draw to the center of the node
+ * then we have to draw the links first and the
+ * draw the boxes on top of them.
+ */
+function elbow(d, direction) {
+  var sourceX = d.source.x,
+      sourceY = d.source.y + (boxWidth / 2),
+      targetX = d.target.x,
+      targetY = d.target.y - (boxWidth / 2);
+
+  return "M" + (direction * sourceY) + "," + sourceX
+    + "H" + (direction * (sourceY + (targetY-sourceY)/2))
+    + "V" + targetX
+    + "H" + (direction * targetY);
+}
+/**
+ * Use a different elbow function for enter
+ * and exit nodes. This is necessary because
+ * the function above assumes that the nodes
+ * are stationary along the x axis.
+ */
+function transitionElbow(d){
+  return "M" + d.source.y + "," + d.source.x
+    + "H" + d.source.y
+    + "V" + d.source.x
+    + "H" + d.source.y;
+}
+
+setup();
 // Use a timer so the chart is not constantly redrawn while window is being resized.
 var resizeTimer;
 window.onresize = function(event) {
@@ -106,6 +473,9 @@ window.onresize = function(event) {
     var s = d3.selectAll('.farm-pedigree svg');
     s = s.remove();
     set_vars();
-    drawGraphic();
+    setup();
   }, 100);
 }
+window.onresize();
+
+}(jQuery));
